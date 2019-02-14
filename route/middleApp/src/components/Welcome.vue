7fc1150a16c71c347d6c5d731b6ab0945cee3f65
weichaozhan <921391554@qq.com>
2019-02-14 13:08:41 +0800

    filter

--- route/middleApp/src/components/Welcome.vue
+++ route/middleApp/src/components/Welcome.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>欢迎使用</div>
+    <div>
+        <h1>
+            欢迎使用
+        </h1>
+        <ul>
+            {{list | test}}
+            <li v-for="(item, index) in list" :key="index" >
+                {{item}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -7,8 +17,13 @@ export default {
     name:'',
     data() {
         return {
-
+            list: [11,2,1,4,454,85,487,21,245,4212,454,57484,1424124,54,7878,4,45,878,942],
         };
+    },
+    filters: {
+        test(list) {
+            return list.filter(item => item < 1000);
+        }
     },
 };
 </script>
